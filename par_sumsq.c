ad5cdb3184014a88b3ba90c2d036446854c7ed7a
Ben Pearson <blpearson44@icloud.com>
2021-05-01 22:58:57 -0700

    okay, I have a working task queue now and I have implemented mutex locks to make sure the threads are not interfering with each other. Going to clean up and make things look pretty as well as the logic more thought out.

--- par_sumsq.c
+++ par_sumsq.c
@@ -20,6 +20,67 @@
 #include <limits.h>
 #include <unistd.h>
 
+// Queue implemented with a singly linked list
+typedef struct node
+{
+  long data;
+  struct node * next;
+}node;
+
+typedef struct queue
+{
+  int count;
+  node * head;
+  node * tail;
+}queue;
+
+void init(queue* q){
+  q->count = 0;
+  q->head = NULL;
+  q->tail = NULL;
+}
+// basic queue operations
+void enqueue(queue *q, long data){
+  node * temp = (node*)malloc(sizeof(node));
+  temp->data = data;
+  if(q->head==NULL){
+    q->head = temp;
+  }
+  else{
+    q->tail->next = temp;
+  }
+  q->tail = temp;
+  q->count++;
+}
+void dequeue(queue *q){
+  if(q->count < 1){
+    return; // check for null
+  }
+  printf("R: %ld\n", q->head->data);
+  node * temp = q->head;
+  q->head = q->head->next;
+  temp->next = NULL;
+  free(temp);
+  q->count--;
+}
+bool is_empty(queue *q){
+  return q->head == NULL;
+}
+
+// to print the queue for testing purposes
+void print_links(node* n){
+  if(n != NULL){
+    printf("P %ld\t", n->data);
+    print_links(n->next);
+  }
+  return;
+}
+void print_queue(queue* q){
+  printf("%d Nodes\n", q->count);
+  print_links(q->head);
+  printf("\n");
+}
+
 
 // shared variables
 long sum = 0;
@@ -29,6 +90,10 @@ long max = INT_MIN;
 bool done = false;
 // this variable will track how many threads are actively being used
 int active_threads = 0;
+queue task_queue;
+
+// thread locker
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void calculate_square(long number){
   long square = number * number;
@@ -48,8 +113,12 @@ void calculate_square(long number){
 }
 
 // Thread function
-void * thr_fn(void* num){
-  long temp = *((long *) num);
+void * thr_fn(){
+  pthread_mutex_lock(&lock);
+  long temp = task_queue.head->data;
+  dequeue(&task_queue);
+  pthread_mutex_unlock(&lock);
+  printf("Data: %ld\n", temp);
   calculate_square(temp);
   active_threads--;
 }
@@ -78,28 +147,43 @@ int main(int argc, char* argv[])
   FILE* fin = fopen(filename, "r");
   char action;
   long num;
-
   int thrs = 0; // for debugging
   while(fscanf(fin, "%c %ld\n", &action, &num) == 2) {
     if (action == 'p'){
       // wait for a thread to be available
-      while(active_threads >= num_threads);
+      // while(active_threads >= num_threads);
       // create thread and have it calculate square
       //printf("Thread %d \n", active_threads);
-      num_arr[active_threads] = num;
-      pthread_create(&thr_arr[active_threads++], NULL, thr_fn, (void *) &num_arr[active_threads]);
+      // num_arr[active_threads] = num;
+      // pthread_create(&thr_arr[active_threads++], NULL, thr_fn, (void *) &num_arr[active_threads]);
+      pthread_mutex_lock(&lock);
+      enqueue(&task_queue, num);
+      pthread_mutex_unlock(&lock);
+      printf("Num: %ld\t Tail: %ld\t", num, task_queue.tail->data);
     }
     else if (action == 'w'){
       //printf("Sleeping\n");
       sleep(num);
     }
     else{
-      //printf("File error\n");
+      printf("File error\n");
       exit(EXIT_FAILURE);
+    }
+    if (active_threads < num_threads && !(is_empty(&task_queue)))
+    {
+      printf("Thr %d\n", active_threads);
+      pthread_create(&thr_arr[active_threads++], NULL, thr_fn, NULL);
+    }
+  }
+  while(!(is_empty(&task_queue))){
+    if(active_threads<num_threads){
+      printf("Thr %d\n", active_threads);
+      pthread_create(&thr_arr[active_threads++], NULL, thr_fn, NULL);
     }
   }
   for(int i = 0; i < num_threads; i++){
     pthread_join(thr_arr[i], NULL);
   }
+  printf("All joined up, Empty: %d\n", is_empty(&task_queue));
   printf("Sum:\t%ld\nOdd:\t%ld\nMin:\t%ld\nMax:\t%ld\n", sum, odd, min, max);
 }
